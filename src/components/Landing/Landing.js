95714284c99bbc465cc03cacc2abe9c64548493f
Josh Dreiling <josh.dreiling77@gmail.com>
2017-10-31 11:35:19 -0700

    added profile information and attempted API

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,15 +1,47 @@
 import React, {Component} from 'react';
 import Particles from 'react-particles-js';
+import axios from 'axios'
+
 
 class Landing extends Component {
+    constructor() {
+        super()
+          this.state = {
+            quote: [],
+          }
+        }
+
+        componentDidMount(){
+            axios.get('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1%27', {
+                headers: {
+                    withCredentials: false,
+                 'Access-Control-Allow-Origin': '*'
+                    }
+              }).then( (response) => {
+                    this.setState({
+                    quote: response.data
+          
+        })
+    })
+    }
     render() {
+
+        var quote = this.state.quote.map((quote) => {
+            return (
+                <div>
+                    {quote.content}
+                    </div>
+            )
+        })
+        console.log(this.state.quote)
         return(
             <div>
                 <div className='landing-title' >
                     <h1>RESTORE</h1>
+        {quote}
                     <h4>Recover. Suppress. Transform. Overcome. Renew. Elevate.</h4>
                 </div>
-                <Particles params={{particles: {line_linked: {shadow: {enable: true, color: "#ffffff", blur: 1}}}}}/>
+                <Particles params={{particles: {line_linked: {shadow: {enable: true, color: "green", blur: 1}}}}}/>
             </div>
         );
     };
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,6 +4,26 @@ class Profile extends Component {
     render() {
         return(
             <div>
+                <div className='profile-box'>
+                    <h3> Username </h3>
+                    <input type='text' placeholder='enter a username...' />
+                    <h3> Addiction type </h3>
+                    <select>
+                    <option value=''>Select</option>
+                    <option value='Alcohol'>Alcohol</option>
+                    <option value='Opiates'>Opiates</option>
+                    <option value='Benzodiazepines'>Benzodiazepines</option>
+                    <option value='Amphetamines'>Amphetamines</option>
+                    <option value='Ecstasy'>Ecstasy</option>
+                    <option value='Barbituates'>Barbituates</option>
+                    <option value='Tobacco'>Tobacco</option>
+                    <option value='Gambling'>Gambling</option>
+                    <option value='Sex'>Sex</option>
+                    <option value='Porn'>Porn</option>
+                    <option value='Other'>Other</option>
+                </select>
+                </div>
+
 
             </div>
 
