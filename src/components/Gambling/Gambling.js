0d53d8ce38864b9a9c0c749300c943dd9ca3d21c
Russell Hayes <info@rahphotographyco.com>
2017-11-01 16:42:42 -0700

    created components

---
+++ src/components/Gambling/Gambling.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Gambling extends React.Component {
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+export default Gambling;
