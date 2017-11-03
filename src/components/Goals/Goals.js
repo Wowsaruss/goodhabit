99fb5734bc180cf56453bf1d7d744afd60914128
Scott Young <scott.t.young1@gmail.com>
2017-11-03 16:25:03 -0700

    goals setting state

--- src/components/Goals/Goals.js
+++ src/components/Goals/Goals.js
@@ -2,7 +2,33 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
 class Goals extends Component {
+        constructor () {
+            super() 
+            this.state = {
+                oneday: "",
+                threeday: "",
+                oneweek: "",
+                twoweek: "",
+                threeweek: "",
+                onemonth: "",
+                twomonth: "",
+                threemonth: "",
+                fourmonth: "",
+                fivemonth: "",
+                sixmonth: "",
+                oneyear: ""
+        }
+    }
+
+    handleGoalInput(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
     render() {
+
+        console.log(this.state)
         return(
             <div>
                 <div className="BGgoals">
@@ -12,18 +38,18 @@ class Goals extends Component {
                         <div className="sidebarGoals"></div>
                         <div className="mainGoals">
                                 <div className="planTitle"><div>Create A Realistic Action Plan For Each Badge</div></div>
-                            <div className="plans"><div className="subGoals"> 1 day Action Plan to Overcome Addiction:</div> <div><textarea type="text"/></div></div>
-                            <div className="plans"><div className="subGoals"> 3 day Action Plan to Overcome Addiction:</div> <div><textarea type="text"/></div></div>
-                            <div className="plans"><div className="subGoals"> 1 Week Action Plan to Overcome Addiction:</div> <div><textarea type="text"/></div></div>
-                            <div className="plans"><div className="subGoals"> 2 Week Action Plan to Overcome Addiction:</div> <div><textarea type="text"/></div></div>
-                            <div className="plans"><div className="subGoals"> 3 Week Action Plan to Overcome Addiction:</div> <div><textarea type="text"/></div></div>
-                            <div className="plans"><div className="subGoals"> 1 Month Action Plan to Overcome Addiction:</div> <div><textarea type="text"/></div></div>
-                            <div className="plans"><div className="subGoals"> 2 Month Action Plan to Overcome Addiction:</div> <div><textarea type="text"/></div></div>
-                            <div className="plans"><div className="subGoals"> 3 Month Action Plan to Overcome Addiction:</div> <div><textarea type="text"/></div></div>
-                            <div className="plans"><div className="subGoals"> 4 Month Action Plan to Overcome Addiction:</div> <div><textarea type="text"/></div></div>
-                            <div className="plans"><div className="subGoals"> 5 Month Action Plan to Overcome Addiction:</div> <div><textarea type="text"/></div></div>
-                            <div className="plans"><div className="subGoals"> 6 Month Action Plan to Overcome Addiction:</div> <div><textarea type="text"/></div></div>
-                            <div className="plans"><div className="subGoals"> 1 Year Action Plan to Overcome Addiction:</div> <div><textarea type="text"/></div></div>
+                            <div className="plans"><div className="subGoals"> 1 day Action Plan to Overcome Addiction:</div> <div><textarea name="oneday" value={this.state.oneday} type="text" onChange={(e) => this.handleGoalInput(e)} /></div></div>
+                            <div className="plans"><div className="subGoals"> 3 day Action Plan to Overcome Addiction:</div> <div><textarea name="threeday" value={this.state.threeday} type="text" onChange={(e) => this.handleGoalInput(e)}/></div></div>
+                            <div className="plans"><div className="subGoals"> 1 Week Action Plan to Overcome Addiction:</div> <div><textarea name="oneweek" value={this.state.oneweek} type="text" onChange={(e) => this.handleGoalInput(e)}/></div></div>
+                            <div className="plans"><div className="subGoals"> 2 Week Action Plan to Overcome Addiction:</div> <div><textarea name="twoweek" value={this.state.twoweek} type="text" onChange={(e) => this.handleGoalInput(e)}/></div></div>
+                            <div className="plans"><div className="subGoals"> 3 Week Action Plan to Overcome Addiction:</div> <div><textarea name="threeweek" value={this.state.threeweek} type="text" onChange={(e) => this.handleGoalInput(e)}/></div></div>
+                            <div className="plans"><div className="subGoals"> 1 Month Action Plan to Overcome Addiction:</div> <div><textarea name="onemonth" value={this.state.onemonth} type="text" onChange={(e) => this.handleGoalInput(e)}/></div></div>
+                            <div className="plans"><div className="subGoals"> 2 Month Action Plan to Overcome Addiction:</div> <div><textarea name="twomonth" value={this.state.twomonth} type="text" onChange={(e) => this.handleGoalInput(e)}/></div></div>
+                            <div className="plans"><div className="subGoals"> 3 Month Action Plan to Overcome Addiction:</div> <div><textarea name="threemonth" value={this.state.threemonth} type="text" onChange={(e) => this.handleGoalInput(e)}/></div></div>
+                            <div className="plans"><div className="subGoals"> 4 Month Action Plan to Overcome Addiction:</div> <div><textarea name="fourmonth" value={this.state.fourmonth} type="text" onChange={(e) => this.handleGoalInput(e)}/></div></div>
+                            <div className="plans"><div className="subGoals"> 5 Month Action Plan to Overcome Addiction:</div> <div><textarea name="fivemonth" value={this.state.fivemonth} type="text" onChange={(e) => this.handleGoalInput(e)}/></div></div>
+                            <div className="plans"><div className="subGoals"> 6 Month Action Plan to Overcome Addiction:</div> <div><textarea name="sixmonth" value={this.state.sixmonth} type="text" onChange={(e) => this.handleGoalInput(e)}/></div></div>
+                            <div className="plans"><div className="subGoals"> 1 Year Action Plan to Overcome Addiction:</div> <div><textarea name="oneyear" value={this.state.oneyear} type="text" onChange={(e) => this.handleGoalInput(e)}/></div></div>
                             <div className="plans2"><div className="planbtn">Submit Goals</div></div>
                         </div>
                     </div>
