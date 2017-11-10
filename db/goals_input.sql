20dd23ecbe6d8f5c93253ae003173e26c8cd7f47
Scott Young <scott.t.young1@gmail.com>
2017-11-10 14:29:09 -0700

    server endpoint changes / sql changes

--- db/goals_input.sql
+++ db/goals_input.sql
@@ -1,5 +1,5 @@
 insert into goals
-(userId, oneday, threeday, oneweek, twoweek, threeweek, onemonth, twomonth, threemonth, fourmonth, fivemonth, sixmonth, oneyear)
+(oneday, threeday, oneweek, twoweek, threeweek, onemonth, twomonth, threemonth, fourmonth, fivemonth, sixmonth, oneyear)
 values
-($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
 RETURNING *;
