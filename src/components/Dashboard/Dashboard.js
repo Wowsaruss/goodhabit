610756439b3d1a8eb280c44e098ab58a44c5f068
Russell Hayes <info@rahphotographyco.com>
2017-11-22 14:50:58 -0700

    Organized JSX in Dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -66,19 +66,28 @@ class Dashboard extends Component {
     }
 
     render() {
-
+/////////////////////////////////////////////////////////////////////////////////////////////
         let journalEntries = this.state.journalEntries.map((journalEntries) => {
             return (
                 <div>
                     <div className="jtitleParent">
-                        <div className="jourDate"><span>Title:&emsp;</span>{journalEntries.journal_title}&emsp;&emsp;<span>Date:&emsp;</span>{journalEntries.journal_date}&emsp;&emsp;<span>Time:&emsp;</span>{journalEntries.journal_time}</div>
-                        <div className="deleteBtn" onClick={(e) => this.handleJournalDelete(journalEntries.id)} > X </div>
+                        <div className="jourDate">
+                            <span>Title:&emsp;</span>
+                            {journalEntries.journal_title}&emsp;&emsp;
+                            <span>Date:&emsp;</span>
+                            {journalEntries.journal_date}&emsp;&emsp;
+                            <span>Time:&emsp;</span>
+                            {journalEntries.journal_time}
+                        </div>
+                        <div className="deleteBtn" onClick={(e) => this.handleJournalDelete(journalEntries.id)} >X</div>
                     </div>
-                        <div className="jourEntries">{journalEntries.journal_entry}</div>
+                    <div className="jourEntries">
+                        {journalEntries.journal_entry}
+                    </div>
                 </div>
             )
         })
-
+/////////////////////////////////////////////////////////////////////////////////////////////
         let goalEntries = this.state.goalEntries.map((goalEntries) => {
             return (
             <div>
@@ -148,6 +157,7 @@ class Dashboard extends Component {
             </div>
             )
         })
+/////////////////////////////////////////////////////////////////////////////////////////////
         let profile = this.state.profile.map(() => {
             return (
                 <div className='user-box/'>
@@ -155,114 +165,199 @@ class Dashboard extends Component {
                  </div>
             )
         })
-
-
+/////////////////////////////////////////////////////////////////////////////////////////////
         return(
-            <div className="dashBG">
-
-                    <div className="bg0Dash">
-                        <div className="dashboard">DASHBOARD</div>
-                    </div>
-                    <div className="Welcome">
-                    {/* <h2>Welcome {profile},</h2> */}
-                        Don't have a profile yet?<br/>Click the link to get started.&ensp;&ensp;&ensp;&ensp;<br/><br/>
-                        <span><Link className="profileLink" to="/profile">Create Profile&ensp;&ensp;</Link></span>
-                        <span><a className="profileLink" href={`${process.env.REACT_APP_HOST}/auth/logout`}>&ensp; / &ensp;Logout</a></span>
-                    </div>
-
-                <div className="quotesParent">
-                    <div className="quotes"><div>{this.state.quote}</div></div>
+            <div>
+                <div className="bg0Dash">
+                    <div className="dashboard">DASHBOARD</div>
                 </div>
-
+                <div className="Welcome">
+                    {/* <h2>Welcome {profile},</h2> */}
+                    Don't have a profile yet?<br/>Click the link to get started.&ensp;&ensp;&ensp;&ensp<br/><br/>
+                    <span><Link className="profileLink" to="/profile">Create Profile&ensp;&ensp;</Link></span>
+                    <span><a className="profileLink" href={`${process.env.REACT_APP_HOST}/auth/logout`}>&ensp; / &ensp;Logout</a></span>
+                </div>
+                <div className="quotesParent">
+                    <div className="quotes">
+                        <div>
+                            {this.state.quote}
+                        </div>
+                    </div>
+                </div>
                 <hr/>
                 <div className="bg1Dash">
                     <div className="badgeParent1">
-                        <div className="badgeName"><div className="badge"><img src={day3} alt=""/></div>3 Days Finished</div>
-                        <div className="badgeName"><div className="badge"><img src={week2} alt=""/></div>2 Weeks Finished</div>
-                        <div className="badgeName"><div className="badge"><img src={month1} alt=""/></div>1 Month Finished</div>
-                        <div className="badgeName"><div className="badge"><img src={month3} alt=""/></div>3 Months Finished</div>
-                        <div className="badgeName"><div className="badge"><img src={month5} alt=""/></div>5 Months Finished</div>
-                        <div className="badgeName"><div className="badge"><img src={year1} alt=""/></div>1 Year Finished</div>
+                        <div className="badgeName">
+                            <div className="badge">
+                                <img src={day3} alt=""/>
+                            </div>
+                            3 Days Finished
+                        </div>
+                        <div className="badgeName">
+                            <div className="badge">
+                                <img src={week2} alt=""/>
+                            </div>
+                            2 Weeks Finished
+                        </div>
+                        <div className="badgeName">
+                            <div className="badge">
+                                <img src={month1} alt=""/>
+                            </div>
+                            1 Month Finished
+                        </div>
+                        <div className="badgeName">
+                            <div className="badge">
+                                <img src={month3} alt=""/>
+                            </div>
+                            3 Months Finished
+                        </div>
+                        <div className="badgeName">
+                            <div className="badge">
+                                <img src={month5} alt=""/>
+                            </div>
+                            5 Months Finished
+                        </div>
+                        <div className="badgeName">
+                            <div className="badge">
+                                <img src={year1} alt=""/>
+                            </div>
+                            1 Year Finished
+                        </div>
                     </div>
-
                     <div className="badgeParent2"> 
-                        <div className="badgeName"><div className="arrow1">&ensp;&ensp;&ensp;&ensp; &#8599;</div><div className="badge">
-                        <img src={day1} alt=""/></div>1 Day Finished</div>
-
-                        <div className="badgeName"><div className="arrow2">&#8600; &ensp;&ensp;&ensp;&ensp;&ensp;</div>
-                        <div className="arrow1">&ensp;&ensp;&ensp;&ensp;&ensp; &#8599;</div><div className="badge">
-                        <img src={week1} alt=""/></div>1 Week Finished</div>
-
-                        <div className="badgeName"><div className="arrow2">&#8600; &ensp;&ensp;&ensp;&ensp;&ensp;</div>
-                        <div className="arrow1">&ensp;&ensp;&ensp;&ensp;&ensp; &#8599;</div><div className="badge">
-                        <img src={week3} alt=""/></div>3 Weeks Finished</div>
-
-                        <div className="badgeName"><div className="arrow2">&#8600; &ensp;&ensp;&ensp;&ensp;&ensp;</div>
-                        <div className="arrow1">&ensp;&ensp;&ensp;&ensp;&ensp; &#8599;</div><div className="badge">
-                        <img src={month2} alt=""/></div>2 Months Finished</div>
-
-                        <div className="badgeName"><div className="arrow2">&#8600; &ensp;&ensp;&ensp;&ensp;&ensp;</div>
-                        <div className="arrow1">&ensp;&ensp;&ensp;&ensp;&ensp; &#8599;</div><div className="badge">
-                        <img src={month4} alt=""/></div>4 Months Finished</div>
-
-                        <div className="badgeName"><div className="arrow2">&#8600; &ensp;&ensp;&ensp;&ensp;&ensp;</div>
-                        <div className="arrow1">&ensp;&ensp;&ensp;&ensp;&ensp; &#8599;</div><div className="badge">
-                        <img src={month6} alt=""/></div>6 Months Finished</div>  
+                        <div className="badgeName">
+                            <div className="arrow1">
+                                &ensp;&ensp;&ensp;&ensp; &#8599;
+                            </div>
+                            <div className="badge">
+                                <img src={day1} alt=""/>
+                            </div>
+                            1 Day Finished
+                        </div>
+                        <div className="badgeName">
+                            <div className="arrow2">
+                                &#8600; &ensp;&ensp;&ensp;&ensp;&ensp;
+                            </div>
+                            <div className="arrow1">
+                                &ensp;&ensp;&ensp;&ensp;&ensp; &#8599;
+                            </div>
+                            <div className="badge">
+                                <img src={week1} alt=""/>
+                            </div>
+                            1 Week Finished
+                        </div>
+                        <div className="badgeName">
+                            <div className="arrow2">
+                                &#8600; &ensp;&ensp;&ensp;&ensp;&ensp;
+                            </div>
+                            <div className="arrow1">
+                                &ensp;&ensp;&ensp;&ensp;&ensp; &#8599;
+                            </div>
+                            <div className="badge">
+                                <img src={week3} alt=""/>
+                            </div>
+                            3 Weeks Finished
+                        </div>
+                        <div className="badgeName">
+                            <div className="arrow2">
+                                &#8600; &ensp;&ensp;&ensp;&ensp;&ensp;
+                            </div>
+                            <div className="arrow1">
+                                &ensp;&ensp;&ensp;&ensp;&ensp; &#8599;
+                            </div>
+                            <div className="badge">
+                                <img src={month2} alt=""/>
+                            </div>
+                            2 Months Finished
+                        </div>
+                        <div className="badgeName">
+                            <div className="arrow2">
+                                &#8600; &ensp;&ensp;&ensp;&ensp;&ensp;
+                            </div>
+                            <div className="arrow1">
+                                &ensp;&ensp;&ensp;&ensp;&ensp; &#8599;
+                            </div>
+                            <div className="badge">
+                                <img src={month4} alt=""/>
+                            </div>
+                            4 Months Finished
+                        </div>
+                        <div className="badgeName">
+                            <div className="arrow2">
+                                &#8600; &ensp;&ensp;&ensp;&ensp;&ensp;
+                            </div>
+                            <div className="arrow1">
+                                &ensp;&ensp;&ensp;&ensp;&ensp; &#8599;
+                            </div>
+                            <div className="badge">
+                                <img src={month6} alt=""/>
+                            </div>
+                            6 Months Finished
+                        </div>  
                     </div>
                 </div>
-
-
-                    <div className="graphParent">
-                        <hr />
-                        <Chart/>
-    
-                        <div className="recoverProgress">
-                            <div className="recoverProgress1">START</div>
-                            <div className="recoverProgress2">SLIP UP</div>
-                        </div>
-
-                        <div className="recoverProgress3">
-                            <div><h3>If you have filled out your profile and are ready to begin your transformation. Click on the <span>"START"</span> button to begin recovering.</h3></div>
-                            <div><h3>If you happen to slip up during your process, hit the <span className="su">"SLIP UP"</span> button. Realize that everything is okay and that mistakes are part of the process of healing.</h3></div>
-                        </div><br/><br/>
+                <div className="graphParent">
+                     <hr />
+                    <Chart/>
+                    <div className="recoverProgress">
+                        <div className="recoverProgress1">START</div>
+                        <div className="recoverProgress2">SLIP UP</div>
                     </div>
-                    
-                            <div className="marginsTitle">
-
-                                <div className="marginsTitle1">Current Goals</div>
-                                <div className="recoverProgress5">
-                                    <h3>Goals are one of the most important parts of a successful transformation and will be a daily reminder to overcome obsticles.</h3> 
-                                    <h3>It is important that before you begin you set realistic goals. To set or change a goal,<br/><Link className="goalLink" to="/goals"><span>Click Here</span></Link></h3>
-                                    <h3>They are methods by which we can strive to better ourselves and replace negative addictions with positive habits.</h3>
-                                </div>
-                                <div className="cGoals">
-                                <div className="overflow">
-                                    <div>{goalEntries}</div>
-                                </div>
-                                </div>
-                        
-                                <div className="marginsTitle1">Journal Entries</div>
-                                <div className="recoverProgress5">
-                                    <h3>Keeping a daily Journal of your progress, struggles and feelings will be a essential record for you.</h3>
-                                    <h3>Be sure to write in your journal everyday. To begin writing about your recovering process.<br/><Link className="goalLink" to="/journal"><span>Click Here</span></Link></h3>
-                                    <h3>An honest Journal aids in the recover process, as it allows an outlet for the feelings that we publically may not want to express.</h3>
-                                </div>
-
-                                
-                                <div className="cJournal">
-                                <div className="overflow">
-                                    <div>{journalEntries}</div>
-                                </div>
-                                </div>
+                    <div className="recoverProgress3">
+                        <div>
+                            <h3>If you have filled out your profile and are ready to begin your transformation. Click on the <span>"START"</span> button to begin recovering.</h3>
+                        </div>
+                        <div>
+                            <h3>If you happen to slip up during your process, hit the <span className="su">"SLIP UP"</span> button. Realize that everything is okay and that mistakes are part of the process of healing.</h3>
+                        </div>
+                    </div>
+                    <br/>
+                    <br/>
+                </div>
+                <div className="marginsTitle">
+                    <div className="marginsTitle1">Current Goals</div>
+                    <div className="recoverProgress5">
+                        <h3>Goals are one of the most important parts of a successful transformation and will be a daily reminder to overcome obsticles.</h3> 
+                        <h3>It is important that before you begin you set realistic goals. To set or change a goal,
+                            <br/>
+                            <Link className="goalLink" to="/goals">
+                                <span>Click Here</span>
+                            </Link>
+                            </h3>
+                        <h3>They are methods by which we can strive to better ourselves and replace negative addictions with positive habits.</h3>
+                    </div>
+                    <div className="cGoals">
+                        <div className="overflow">
+                            <div>{goalEntries}</div>
+                        </div>
+                    </div>
+                    <div className="marginsTitle1">Journal Entries</div>
+                    <div className="recoverProgress5">
+                        <h3>Keeping a daily Journal of your progress, struggles and feelings will be a essential record for you.</h3>
+                        <h3>Be sure to write in your journal everyday. To begin writing about your recovering process.<br/>
+                            <Link className="goalLink" to="/journal">
+                                <span>Click Here</span>
+                            </Link>
+                        </h3>
+                        <h3>An honest Journal aids in the recover process, as it allows an outlet for the feelings that we publically may not want to express.</h3>
+                    </div>
+                    <div className="cJournal">
+                        <div className="overflow">
+                            <div>
+                                {journalEntries}
                             </div>
-                    
-                    
-                        <div className="footerDash">
-                            <Link to="/goals" className="LinkD"><div className="footerDashGoals"> Goals </div></Link>
-                            <Link to="/journal" className="LinkD"><div className="footerDashGoals"> Journal </div></Link>
-                        </div>  
+                        </div>
+                    </div>
+                </div>
+                <div className="footerDash">
+                    <Link to="/goals" className="LinkD">
+                        <div className="footerDashGoals">Goals</div>
+                    </Link>
+                    <Link to="/journal" className="LinkD">
+                        <div className="footerDashGoals">Journal</div>
+                    </Link>
+                </div>  
             </div>
-
         )
     }
 }
